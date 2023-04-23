60addf4c30a14a822de8c63f94fe315ace8e96c8
mostafahmed <mostafa.hamed@tagaddod.com>
2023-04-23 02:06:25 +0200

    add app_env

--- src/config/google.js
+++ src/config/google.js
@@ -3,7 +3,11 @@ const User = require("../../models/user");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const UserService = require('../user')
 
-const callbackURL = "http://localhost:81/auth/google/callback";
+if (process.env.APP_ENVIRONMENT === "development") {
+  const callbackURL = "http://localhost:81/auth/google/callback";
+}else{
+  const callbackURL = "http://54.208.184.10/auth/google/callback";
+}
 passport.use(
   new GoogleStrategy(
     {
